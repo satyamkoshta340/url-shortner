f94e001981b633af49febf0f8b9f334cf4e4d271
satyamkoshta340 <satyamkoshta340@gmail.com>
2026-05-18 00:15:17 +0530

    code setup

---
+++ app/db.py
@@ -0,0 +1,12 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, DeclarativeBase
+
+DATABASE_URL = "sqlite:///./app.db"
+
+engine = create_engine(DATABASE_URL)
+
+SessionLocal = sessionmaker(bind=engine)
+
+
+class Base(DeclarativeBase):
+    pass
